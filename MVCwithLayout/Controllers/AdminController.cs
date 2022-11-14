9be000674a4343737f53065a17c5cba603cede7d
SEM <SEM@DESKTOP-4GKM6HK>
2022-11-14 13:00:48 +0300

    Add project files.

---
+++ MVCwithLayout/Controllers/AdminController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCwithLayout.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Islem()
+        {
+            return View();
+        }
+        public IActionResult Mail()
+        {
+            return View();
+        }
+    }
+}
